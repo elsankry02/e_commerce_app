a9d2cfcee227f3d10b8234509f366cb4d3a22dc4
elsankry02 <githup800900@gmail.com>
2026-01-06 20:33:51 +0200

    feat: add AppColors

---
+++ lib/core/constants/app_colors.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const kError = Color(0xfff95555);
+  static const kWhite = Color(0xffFFFFFF);
+  static const kMainColor = Color(0xff004182);
+  // Text Colors
+  static const kTextColor = Color(0xff06004F);
+  static const kGrayColor = Color(0xff808080);
+  static const kDarkBlue = Color(0xB2000000);
+  static const kDescriptionColor = Color(0xff004f99);
+  // Product Details Colors
+  static const kOrange = Color(0xffFF645A);
+  static const kGreen = Color(0xff02B935);
+  static const kBlue = Color(0xff0973DD);
+  static const kRed = Color(0xffBC3018);
+  static const kBrowen = Color(0xff2F2929);
+}
