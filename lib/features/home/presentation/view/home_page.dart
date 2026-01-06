3d2d3069b8de666f3f6f36893e96cd59bd950acc
elsankry02 <githup800900@gmail.com>
2026-01-06 20:34:29 +0200

    fix: home Page

--- lib/features/home/presentation/view/home_page.dart
+++ lib/features/home/presentation/view/home_page.dart
@@ -1,19 +1,20 @@
 import 'package:auto_route/annotations.dart';
+import 'package:e_commerce_app/core/constants/app_colors.dart';
 import 'package:flutter/material.dart';
 
 @RoutePage()
-class HomePage extends StatefulWidget {
+class HomePage extends StatelessWidget {
   const HomePage({super.key});
-
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Route E-Commerce App")),
+      appBar: AppBar(
+        backgroundColor: AppColors.kMainColor,
+        title: Text(
+          "E Commerce App",
+          style: TextStyle(color: AppColors.kWhite),
+        ),
+      ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
