c352e2211581746b2ef6cde7d2c3103a55fc6c2d
elsankry02 <githup800900@gmail.com>
2026-01-06 17:28:52 +0200

    feat: download package auto router and use it

--- lib/app.dart
+++ lib/app.dart
@@ -1,3 +1,4 @@
+import 'package:e_commerce_app/core/router/router.dart';
 import 'package:flutter/material.dart';
 
 class EcommerceApp extends StatelessWidget {
@@ -5,28 +6,10 @@ class EcommerceApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(),
-      home: const MyHomePage(title: "Route E-Commerce App"),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text(widget.title)),
-      body: Column(mainAxisAlignment: MainAxisAlignment.center, children: []),
+    return MaterialApp.router(
+      routerConfig: router.config(),
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(brightness: Brightness.light),
     );
   }
 }
--- lib/core/router/router.dart
+++ lib/core/router/router.dart
@@ -1,1 +1,14 @@
-// part 'router.gr.dart';
+import 'package:auto_route/auto_route.dart';
+import 'package:e_commerce_app/features/home/presentation/view/home_page.dart';
+
+part 'router.gr.dart';
+
+final router = AppRouter();
+
+@AutoRouterConfig()
+class AppRouter extends RootStackRouter {
+  @override
+  List<AutoRoute> get routes => [
+    AutoRoute(page: HomeRoute.page, initial: true),
+  ];
+}
