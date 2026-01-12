8fde2e3ae3a7fdd23f99f85543f27dfd320cd745
elsankry02 <githup800900@gmail.com>
2026-01-12 09:31:49 +0200

    feat: refactor app structure and add custom components

--- lib/app.dart
+++ lib/app.dart
@@ -1,5 +1,4 @@
-import 'package:e_commerce_app/core/constants/app_strings.dart';
-import 'package:e_commerce_app/core/router/router.dart';
+import 'package:e_commerce_app/core/utils/app_strings.dart';
 import 'package:flutter/material.dart';
 
 class EcommerceApp extends StatelessWidget {
@@ -7,8 +6,7 @@ class EcommerceApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      routerConfig: router.config(),
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(brightness: Brightness.light, fontFamily: kPoppins),
     );
