bc2ce8b5c7af4545308ffdce3af211cf54d8b6e5
elsankry02 <githup800900@gmail.com>
2026-01-15 10:18:16 +0200

    feat: add CustomHomeCircleItem widget and associated image for enhanced category display

---
+++ lib/core/widgets/custom_home_circle_item.dart
@@ -0,0 +1,58 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:e_commerce_app/core/utils/constants/app_images.dart';
+import 'package:e_commerce_app/core/utils/extensions/app_extensions.dart';
+import 'package:e_commerce_app/core/utils/theme/app_colors.dart';
+import 'package:e_commerce_app/core/utils/theme/app_text_style.dart';
+import 'package:flutter/material.dart';
+
+class CustomHomeCircleItem extends StatelessWidget {
+  final String imageUrl, title;
+  const CustomHomeCircleItem({
+    super.key,
+    required this.imageUrl,
+    this.title = "Category",
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final border = BorderRadius.circular(5);
+    final double imageSize = context.kHeight * 0.12;
+    return Column(
+      spacing: 8,
+      mainAxisSize: .min,
+      mainAxisAlignment: .center,
+      children: [
+        InkWell(
+          borderRadius: border,
+          child: ClipOval(
+            child: CachedNetworkImage(
+              imageUrl: imageUrl,
+              fit: .cover,
+              height: imageSize,
+              width: imageSize,
+              errorWidget: (context, url, error) => Image.asset(
+                AppImages.kHomeCircleItem,
+                fit: .cover,
+                height: imageSize,
+                width: imageSize,
+              ),
+            ),
+          ),
+        ),
+        SizedBox(
+          width: imageSize,
+          child: Text(
+            title,
+            textAlign: .center,
+            maxLines: 1,
+            overflow: .ellipsis,
+            style: AppTextStyle.kTitleSmallRegular(
+              context,
+              color: AppColors.kTextColor,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
